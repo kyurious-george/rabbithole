20798746dfc2344d104bbcbdae3f28807eaa5d3a
Kyumin Park <kyumin.park@shopify.com>
2026-06-06 04:09:44 -0400

    begin the journey of an llm:

---
+++ build-an-llm/appendix_a.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn.functional as F
+
+y = torch.tensor([1.0])
+x1 = torch.tensor([1.1])
+w1 = torch.tensor([2.2], requires_grad=True)
+b = torch.tensor([0.0], requires_grad=True)
+
+z = x1 * w1 + b
+a = torch.sigmoid(z)
+
+loss = F.binary_cross_entropy(a, y)
+
+loss.backward()
+print(w1.grad)
+print(b.grad)
